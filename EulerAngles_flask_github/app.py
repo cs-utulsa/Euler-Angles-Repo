d6948b0d4042833374b427878271b1541a4a3be4
Yao Wang <oscarwangy@gmail.com>
2022-10-30 22:45:48 -0500

    Navigation Bar Added
    A dynamic navigation bar has been added to enable jumping between pages. Replaces the previous method of changing the domain name redirect page.

--- EulerAngles_flask_github/app.py
+++ EulerAngles_flask_github/app.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/help/')
-def about():
+@app.route('/help')
+def help():
     return render_template('help.html')
 
-@app.route('/Converter/')
-def EulerToQuaternion():
-        return render_template('EulerToQuaternion.html')
+@app.route('/converter')
+def euler():
+    return render_template('EulerToQuaternion.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
