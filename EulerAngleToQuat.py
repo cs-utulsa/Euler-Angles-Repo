0e283404f61da36879789aaa6641f0a53e96c910
samuel-ricketts <78981392+samuel-ricketts@users.noreply.github.com>
2022-11-01 22:52:35 -0500

    update method

--- EulerAngleToQuat.py
+++ EulerAngleToQuat.py
@@ -4,4 +4,4 @@ def eulerToQuat(x, y, z, rotationSel):
     Rot = R.from_euler(rotationSel, [x, y, z], degrees=True)
     RotArray = Rot.as_matrix() 
     Rot_quat = Rot.as_quat()
-    return Rot_quat
+    return str(Rot_quat)
