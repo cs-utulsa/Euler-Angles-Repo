22620dc16095c751cb8187c8e2fdc6150b1c325a
ktj4667 <ktj4667@utulsa.edu>
2023-02-28 22:14:32 -0600

    Initial Determinant Calculator

---
+++ BootstrapWorkFolder/HTMLs/conversions/Determinant.js
@@ -0,0 +1,17 @@
+//start with a 3x3 and expand later
+//expand to include inverse
+//how might we show what is going on?
+
+function matrix_determinant(){
+
+    const M11 = document.getElementById("M11")
+    const M12 = document.getElementById("M12")
+    
+
+    const matrix = math.matrix([0, 1, 2], [4, 5, 6], [7, 8, 9])
+
+    const determinant = math.det(matrix)
+
+
+}
+
