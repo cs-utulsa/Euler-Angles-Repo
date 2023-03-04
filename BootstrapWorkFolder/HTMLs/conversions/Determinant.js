c64012157d7f6005c06e8faba7561eadd02c1440
ktj4667 <ktj4667@utulsa.edu>
2023-03-04 17:48:25 -0600

    additions to determinant

--- BootstrapWorkFolder/HTMLs/conversions/Determinant.js
+++ BootstrapWorkFolder/HTMLs/conversions/Determinant.js
@@ -1,17 +1,31 @@
-//start with a 3x3 and expand later
-//expand to include inverse
-//how might we show what is going on?
+//max out at 10
+
+var size = 2
 
 function matrix_determinant(){
 
+
+    switch(size){
+        //if doing a ten by ten, we can't have 100 of these...
+        //listen for an array??
+        case(2):
+        const M11 = document.getElementById("M11")
+        const M12 = document.getElementById("M12")
+        const M21 = document.getElementById("M21")
+        const M22 = document.getElementById("M22")
+        const matrix = math.matrix([0, 1, 2], [4, 5, 6], [7, 8, 9])
+    }
+
     const M11 = document.getElementById("M11")
     const M12 = document.getElementById("M12")
+    const M13 = document.getElementById("M13")
     
 
-    const matrix = math.matrix([0, 1, 2], [4, 5, 6], [7, 8, 9])
 
     const determinant = math.det(matrix)
 
 
 }
+
+
 
