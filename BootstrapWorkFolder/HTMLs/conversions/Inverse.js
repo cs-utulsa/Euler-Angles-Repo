6d1fc2d2540397b785a1fd0a65957acc4f8c38de
ktj4667 <ktj4667@utulsa.edu>
2023-04-06 13:37:14 -0500

    Inverse Calculator (Not working yet)

---
+++ BootstrapWorkFolder/HTMLs/conversions/Inverse.js
@@ -0,0 +1,88 @@
+//UI part
+const inverseArea = document.getElementById("inverseArea");
+const goButton = document.getElementById("goButton");
+
+goButton.addEventListener("click", makeGrid);
+
+function makeGrid() {
+    var size = document.getElementById("userInput").value;
+
+    while(inverseArea.firstChild){
+        inverseArea.removeChild(inverseArea.firstChild);
+    }
+
+    if(size < 2 || size > 10) {
+        inverseArea.innerHTML = "Please Enter A Valid Size.";
+    } else {
+        for(let i = 0; i < size; i++) {
+            for(let j = 0; j < size; j++) {
+                let node = document.createElement("input");
+                node.setAttribute("name", "matrixValue");
+                node.setAttribute("type", "text");
+                node.setAttribute("id", "in" + i + "_" + j);
+                node.setAttribute("size", 4);
+                inverseArea.appendChild(node);
+            }
+            inverseArea.appendChild(document.createElement("br"));
+    
+        }
+        
+    }
+}
+
+//Calculations
+
+const calcButton = document.getElementById("calculate");
+const closeButton = document.getElementById("closeModal");
+const answerBox = document.getElementById("answerBox");
+closeButton.addEventListener("click", reset);
+calcButton.addEventListener("click", matrix_inverse);
+var calcCount = 0;
+
+function matrix_inverse(){
+    
+    var matrix_values = document.getElementsByName("matrixValue");
+    var num_rows = document.getElementById("userInput").value;
+    var rows_int = parseInt(num_rows);
+    const m = new THREE.Matrix4();
+
+    m.fromArray(matrix_values.values);
+
+    /** 
+    A.resize([rows_int, rows_int]);
+    var k = 0;
+    for(i = 0; i < num_rows; i++){
+        for(j = 0; j < num_rows; j++) {
+            A.subset(math.index(i, j), matrix_values[k].value);
+            k++;
+        }
+    }
+    */
+
+    //if inverse doesn't exist??
+    const inverse = m.invert();
+    
+    let output = document.createTextNode("Inverse:   ");
+    let num = document.createTextNode(inverse);
+
+    if(calcCount > 0) {
+        while(answerBox.firstChild){
+            answerBox.removeChild(answerBox.firstChild);
+        }
+    }
+
+    answerBox.appendChild(output);
+    answerBox.appendChild(num);
+
+    calcCount++;
+
+
+}
+
+function reset() {
+    calcCount = 0;
+
+    while(answerBox.firstChild){
+        answerBox.removeChild(answerBox.firstChild);
+    }
+}
